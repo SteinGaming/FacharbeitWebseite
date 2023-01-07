c6912bd027a439733066bb85c83c78db7c4eecdc
SteinGaming <danialdaryab@gmail.com>
2023-01-07 20:46:40 +0100

    fix: optimize examples + add footer

--- js/example.js
+++ js/example.js
@@ -4,67 +4,46 @@ $(
     function () {
         for (let i = 0; i < list.length; i++) {
             const element = list[i];
-            let li = document.createElement('li')
-            li.style.opacity = "0"
-            li.id = element
-            let li$ = $(li) // jQuery object
+            let li = $('<li style="opacity: 0;" id=' + element + ' ></li>') // jQuery object
 
 
             $("#examples").append(li)
             jQuery.ajax({
                     url: "./media/examples/" + element + ".md",
                     success: async function (data) {
-                        li$.append(
+                        li.append(
                             marked.parse(data)
                         )
                         $("#" + element + " pre").each(function (index) {
-                            let buttonDiv = document.createElement('div')
-                            buttonDiv.className = "buttonDiv hljs"
+                            let buttonDiv = $('<div class="buttonDiv hljs"></div>')
                             addButton(buttonDiv)
-                            this.prepend(buttonDiv)
+                            $(this).prepend(buttonDiv)
                             let code = this.getElementsByTagName("code")[0]
                             code.classList.add("hljs")
                             code.innerHTML = hljs.highlightAuto(code.innerHTML.replaceAll(/&lt;/g, "<").replaceAll(/&gt;/g, ">")).value
+                            $("a", li).attr("target", "_blank") // Replace all links to create new tabs AFTER highlighting is done to prevent issues
                         })
-                        li$.delay(1000).animate({opacity: 1}, 750, function () {
-                            post(element)
-                        })
+                        li.delay(1000).animate({opacity: 1}, 750)
                     }
                 }
             )
         }
 
         function addButton(element) {
-            let button = document.createElement('button')
+            let button = $('<button class="expand">+</button>')
             //button.id = element
-            button.className = "expand"
-            button.innerText = "+"
-            button.onclick = function () {
+            button.click(function () {
                 const htmlElement = this.parentNode;
                 import("./modal.js").then((module) => {
                     let preNode = htmlElement.parentNode.cloneNode(true)
                     preNode.getElementsByClassName("buttonDiv")[0].remove()
-                    preNode.getElementsByTagName("code")[0].prepend(
-                        module.createModal(preNode)
-                    )
+                    $(preNode.getElementsByTagName("code")[0]).prepend(module.createModal(preNode))
                 })
-            }
-            element.prepend(
+            })
+            element.append(
                 button
             )
             return button
         }
     }
 )
-
-const checkList = list.concat()
-
-// Only run, if all elements are done loading
-function post(element) {
-    if (checkList.indexOf(element) !== undefined) {
-        checkList.splice(checkList.indexOf(element), 1)
-    }
-    if (checkList.length === 0) {
-        $("#content a").attr("target", "_blank")
-    }
-}
