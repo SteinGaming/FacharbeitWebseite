34a36404c1b86cb927c06d94f4c0e2c1727b927f
SteinGaming <danialdaryab@gmail.com>
2023-01-07 20:52:58 +0100

    feat: modal manager (only used in examples)

--- js/modal.js
+++ js/modal.js
@@ -1,25 +1,19 @@
 // From: https://www.w3schools.com/howto/howto_css_modals.asp
 
 export function createModal(content) {
-    const modal = document.createElement("div");
-    modal.style.opacity = "0";
+    content.classList.add("modal-content");
+    const modal = $("<div></div>").attr("class", "modal").attr("style", "opacity: 0;").append(content)
     function animateClose() {
-        $(modal).animate({ opacity: 0 }, 500, function () {
+        modal.animate({ opacity: 0 }, 500, function () {
             modal.remove()
         })
     }
-    modal.className = "modal";
-    content.classList.add("modal-content");
-    modal.appendChild(content)
-    document.body.appendChild(modal);
-    const closeButton = document.createElement("span");
-    closeButton.className = "close";
-    closeButton.innerHTML = "&times;";
-    closeButton.onclick = function () {
+    $(document.body).append(modal);
+    const closeButton = $("<span class='close'>&times;</span>").click(function () {
         animateClose()
-    }
+    })
     document.body.onclick = function (event) {
-        if (event.target === modal) {
+        if (event.target.classList.contains("modal")) {
             animateClose()
         }
     }
